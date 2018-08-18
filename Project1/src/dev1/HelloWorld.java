edeb14d51a3dc4e32f0c7de38bfe3667a3e1127b
Marco Colasito <mtcolasito@valdosta.edu>
2018-08-17 22:45:42 -0400

    Added a surprise

--- Project1/src/dev1/HelloWorld.java
+++ Project1/src/dev1/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Suh dude :P");
+		System.out.printf("%3d viruses installed", (int)(Math.random() * 1000));
 	}
 
 }

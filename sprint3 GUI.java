1e46c742d3f31a12c1d14bfd9206df9799ff3a79
Earlusion <36013206+Earlusion@users.noreply.github.com>
2018-11-29 18:52:05 -0500

    Update sprint3 GUI.java

--- sprint3 GUI.java
+++ sprint3 GUI.java
@@ -1,184 +1,160 @@
 package application;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.geometry.Pos;
+import javafx.geometry.Insets;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.VBox;
-import javafx.scene.layout.StackPane;
 import javafx.scene.control.Button;
-
 import javafx.scene.control.Label;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
+import javafx.scene.text.Font;
 
 public class Main extends Application {
-	//Initialize tabs
-	TabPane tp = new TabPane();
-	BorderPane brdPane = new BorderPane();
-	
-	int height = 1000;
-	int length = 250;
-
-	// define all GUI controls as data fields
-	Button btnAdd, btnSubtract, btnMultiply, btnDivide, btnExponent, btnLoad;
-	Label lbl1, lbl2, lbl3;
-	TextField txfValue1, txfValue2;
-	TextArea txa;
-	TabPane tabs;
-
-	Scene scene1, scene2, scene3;
-	GridPane gp1, gp2;
-
-	//Runs the GUI
-		public static void main(String[] args) {
-			launch(args);
-		}
+	Scene scene1, scene2, scene3, scene4;
 
 	@Override
 	public void start(Stage primaryStage) {
-		Pane grdRootPane = buildGuiPane();
+		primaryStage.setTitle("Some Random Ass Website Forum");
 
-        //Page 1
-		Tab memTab = new Tab();		
-			gp1 = new GridPane();
-			gp1.setHgap(10);
-			gp1.setVgap(10);
-			
-			scene1 = new Scene(gp1, 240, 100);
+		//Member Page
+		GridPane gp = new GridPane();
+		gp.setHgap(10);
+		gp.setVgap(10);
+		gp.setPadding(new Insets(10, 10, 10, 10));
 
-			//Text Field
-			TextField txf1 = new TextField();
-			
-			//Button1
-			Button b1 = new Button("Create Account");
-			
-			gp1.add(b1, 1, 0, 2, 1);
-			gp1.add(txf1, 0, 0);
-			
-		//Page 2
-		Tab groupTab = new Tab();
-			gp2 = new GridPane();
-			gp2.setHgap(10);
-			gp2.setVgap(10);
-			
-			scene2 = new Scene(grdRootPane, 240, 100);
-			
-		tabs.getTabs().addAll(memTab, groupTab);
-			
-		primaryStage.
+		Button home1 = new Button("Home");
+		home1.setOnAction(e -> primaryStage.setScene(scene1));
 
-        primaryStage.setScene(scene1);
-        primaryStage.setTitle("Title here");
-        primaryStage.show();
+		Label title= new Label("Welcome New Members!");
+		title.setFont(new Font("Comic Sans", 46));
+		gp.add(title, 1, 0);
 
-/*
-		Scene scene = new Scene(grdRootPane,475, 275);
-		primaryStage.setScene(scene);
-		primaryStage.setTitle("MyLittleCalculator");
-		*/
+		Label fnameTxt = new Label("First Name: ");
+		TextField fname = new TextField();
+		fname.setMaxWidth(150);
+		gp.add(fnameTxt, 0, 1, 1, 1);
+		gp.add(fname, 1, 1, 1, 1);
+
+		Label lnameTxt = new Label("Last Name: ");
+		TextField lname = new TextField();
+		lname.setMaxWidth(150);
+		gp.add(lnameTxt, 0, 2, 1, 1);
+		gp.add(lname, 1, 2, 1, 1);
+
+		Label screenNameTxt = new Label("Screen Name: ");
+		TextField screenName = new TextField();
+		screenName.setMaxWidth(150);
+		gp.add(screenNameTxt, 0, 3, 1, 1);
+		gp.add(screenName, 1, 3, 1, 1);
+
+		Label emailTxt = new Label("Email Address: ");
+		TextField email = new TextField();
+		email.setMaxWidth(250);
+		gp.add(emailTxt, 0, 4, 1, 1);
+		gp.add(email, 1, 4, 1, 1);
+
+		Button button1 = new Button("Create Account");
+		gp.add(button1, 1, 5);
+		gp.add(home1, 0, 0);
+
+		button1.setOnAction(e -> primaryStage.setScene(scene2));
+
+		scene1= new Scene(gp, 750, 1000);
+
+
+		//Group Page
+		GridPane gp2 = new GridPane();
+		gp2.setHgap(10);
+		gp2.setVgap(10);
+		gp2.setPadding(new Insets(10, 10, 10, 10));
+
+		Label title2= new Label("Create New Group");
+		title2.setFont(new Font("Comic Sans", 32));
+		gp2.add(title2, 1, 0);
+
+		Label gTitleTxt = new Label("Group Title: ");
+		TextField gTitle = new TextField();
+		fname.setMaxWidth(150);
+		gp2.add(gTitleTxt, 0, 1, 1, 1);
+		gp2.add(gTitle, 1, 1, 1, 1);
+
+		Label descTxt = new Label("Description: ");
+		TextArea desc = new TextArea();
+		lname.setMaxWidth(150);
+		gp2.add(descTxt, 0, 2, 1, 1);
+		gp2.add(desc, 1, 2, 1, 1);
+
+		Button button2= new Button("Create Group");
+		button2.setOnAction(e -> primaryStage.setScene(scene1));
+		gp2.add(button2, 1, 4);
+
+		Button button3= new Button("Posts Page");
+		button3.setOnAction(e -> primaryStage.setScene(scene3));
+
+		Button home2 = new Button("Home");
+		home2.setOnAction(e -> primaryStage.setScene(scene1));
+
+		gp2.add(button3, 1, 7);
+		gp2.add(home2, 0, 0);
+
+		scene2= new Scene(gp2, 750, 1000);
+
+		//Posts Page
+		GridPane gp3 = new GridPane();
+		gp3.setHgap(10);
+		gp3.setVgap(10);
+		gp3.setPadding(new Insets(10, 10, 10, 10));
+
+		Button home3 = new Button("Home");
+		home3.setOnAction(e -> primaryStage.setScene(scene1));
+
+		Button goToQuestions = new Button("Search");
+		goToQuestions.setOnAction(e -> primaryStage.setScene(scene4));
+		gp3.add(goToQuestions, 4, 7);
+
+		Label title3= new Label("Post a Question");
+		title3.setFont(new Font("Comic Sans", 32));
+		gp3.add(title3, 1, 0);
+
+		gp3.add(home3, 0, 0);
+
+		scene3 = new Scene(gp3, 750, 1000);
+
+		//Question Page
+		GridPane gp4 = new GridPane();
+		gp4.setHgap(10);
+		gp4.setVgap(10);
+		gp4.setPadding(new Insets(10, 10, 10, 10));
+
+		Button home4 = new Button("Home");
+		home4.setOnAction(e -> primaryStage.setScene(scene1));
+
+		Label questionTxt = new Label("Question: ");
+		TextArea question = new TextArea();
+		question.setEditable(false);
+		gp4.add(question, 1, 1);
+		gp4.add(questionTxt, 0, 1);
+
+		Label answerTxt = new Label("Post an answer: ");
+		TextArea answer = new TextArea();
+		gp4.add(answerTxt, 0, 2);
+		gp4.add(answer, 1, 2);
+
+		Button postAns = new Button("Post Answer");
+		//postAns.setOnAction(e -> primaryStage.setScene(scene1));
+
+		gp4.add(postAns, 2, 2);
+
+		scene4 = new Scene(gp4, 750, 1000);
+
+		//ESKETTITTTT
+		primaryStage.setScene(scene1);
 		primaryStage.show();
 	}
 
-	
-	private Pane buildGuiPane() {
-		//Create all necessary buttons and text fields/areas
-		btnAdd = new Button("add");
-		btnSubtract = new Button("subtract");
-		btnMultiply= new Button("multiply");
-		btnDivide = new Button("divide");
-		btnLoad = new Button("load");
-		btnExponent = new Button("exp");
-		lbl1 = new Label("Operand 1: ");
-		lbl2 = new Label("Operand 2: ");
-		lbl3 = new Label("Result: ");
-		txfValue1 = new TextField();
-		txfValue2 = new TextField();
-		txa = new TextArea();
-		txa.setEditable(false);
-		txa.setPrefColumnCount(30);
-		txa.setPrefRowCount(5);
-		//We use gridPane1 to implement the first row of the calculator
-		GridPane gridPane1 = new GridPane();
-		gridPane1.setAlignment(Pos.CENTER);
-		gridPane1.setHgap(200);
-		gridPane1.setVgap(5);
-		gridPane1.add(lbl1, 0, 0);
-		gridPane1.add(lbl2, 0, 1);
-		gridPane1.add(txfValue1, 1, 0);
-		gridPane1.add(txfValue2, 1, 1);
-		//We use gridPane2 to implement the second row of the calculator
-		GridPane gridPane2 = new GridPane();
-		gridPane2.setAlignment(Pos.CENTER);
-		gridPane2.setHgap(20);
-		//Add GUI controls to gridPane2
-		gridPane2.add(btnAdd, 0, 0);
-		gridPane2.add(btnSubtract, 1, 0);
-		gridPane2.add(btnMultiply, 2, 0);
-		gridPane2.add(btnDivide, 3, 0);
-		gridPane2.add(btnLoad, 4, 0);
-		gridPane2.add(btnExponent, 5, 0);
-		//We use gridPane3 to implement the last row of the calculator
-		GridPane gridPane3 = new GridPane();
-		gridPane3.setAlignment(Pos.CENTER);
-		gridPane3.setHgap(10);
-		//Add GUI controls to gridPane3
-		gridPane3.add(lbl3, 0, 0);
-		gridPane3.add(txa, 1, 0);
-		// We create our top-level pane here.
-		GridPane topPane = new GridPane();
-		topPane.setAlignment(Pos.CENTER);
-		topPane.setVgap(20);
-		//Here we add gridPane1, gridPane2 and gridPane3 to our topPane, which is our final layout
-		topPane.add(gridPane1, 0, 0);
-		topPane.add(gridPane2, 0, 1);
-		topPane.add(gridPane3, 0, 2);
-		//Register all the buttons to the same event handler function named "calculate"
-		btnAdd.setOnAction(e -> {calculate(e);});
-		btnSubtract.setOnAction(e -> {calculate(e);});
-		btnMultiply.setOnAction(e -> {calculate(e);});
-		btnDivide.setOnAction(e -> {calculate(e);});
-		btnLoad.setOnAction(e -> {calculate(e);});
-		btnExponent.setOnAction(e -> {calculate(e);});
-		return topPane;
+	public static void main(String[] args) {
+		launch(args);
 	}
-	//Method used to execute actions for respective buttons
-	public void calculate(ActionEvent e){
-		double num1 = Double.parseDouble(txfValue1.getText());
-		double num2 = Double.parseDouble(txfValue2.getText());
-		double tot = 0;
-		//Implements the button-clicking event handler
-		if(e.getSource() == btnAdd){
-			tot = num1 + num2;
-			txa.setText(String.format("%.2f", tot));
-		}
-		else if(e.getSource() == btnSubtract){
-			tot = num1 - num2;
-			txa.setText(String.format("%.2f", tot));
-		}
-		else if(e.getSource() == btnMultiply){
-			tot = num1 * num2;
-			txa.setText(String.format("%.2f", tot));
-		}
-		else if(e.getSource() == btnDivide){
-			tot = num1 / num2;
-			txa.setText(String.format("%.2f", tot));
-		}
-		else if(e.getSource() == btnLoad){
-			txfValue1.setText(txa.getText());
-			txa.setText(null);
-		}
-		else if(e.getSource() == btnExponent){
-			tot = Math.pow(num1, num2);
-			txa.setText(String.format("%.2f", tot));
-		}
-	}
-	
-
 }
